7b8a818a03bfb688bbcc098adf055a7581c12083
ChloeRNater <chloe.nater@nina.no>
2023-09-22 15:01:31 +0200

    Complete wrapper for processing PECBMS outputs

--- R/processRtrimOutput_PECBMS.r
+++ R/processRtrimOutput_PECBMS.r
@@ -1,4 +1,19 @@
+#' Process results from PECBMS Trim analysis
+#'
+#' @param folder character. Path to directory which contains the RTRIM argument
+#' and output files, and into which to save outputs.
+#'
+#' @return a dataframe containing collated results from PECBMS Trim analyses
+#' for all species and years.
+#' @export
+#'
+#' @examples
+
 processRtrimOutput_PECBMS <- function(folder){
+  
+  # ----------------------------------------------------------------------------
+  # START OF CODE FROM PECBMS
+  #-----------------------------------------------------------------------------
   
   #####################################################################################################################
   # RTRIM-shell second script. Version RTRIM-shell_v1.7
@@ -13,19 +28,7 @@ processRtrimOutput_PECBMS <- function(folder){
   #####################################################################################################################
   # PREPARATION. 
   #####################################################################################################################
-  rm(list=ls())                                                                       # 1/06/2020 Eva Silarova unified upper and lower cases in variable names - v1.1
-  
-  # Link to script with functions   !! user dependent !! !!Since the scripts are numbered, enter x3_functions_Shell_Rtrim.R!!
-  source("D://RTRIM-shell//02_RTRIM_shell_v1.7_scripts//shell//x3_functions_Shell_Rtrim.R") #Adjust the path to your working folder according to your computer (try different slashes, if double backslash does not work)
-  # Do not forget to set script with functions x3_functions_Shell_Rtrim.R at the end of path!!!
-  # The required library
-  library(rtrim)
-  
-  # Select the directory which contains the RTRIM output files
-  # This directory also contains the files with arguments used in the rtrim function.
-  folder <- "D://RTRIM-shell//02_RTRIM_shell_v1.7_scripts//shell"  # !! user dependent!! 
-  # folder <- choose.dir(default ="C://your_path_to_working_folder_with_input_data", caption = "Select directory which contains data which has been analysed")
-  setwd(folder)
+
   
   #####################################################################################################################
   # SELECTING SUCCESSFUL RUNS.
@@ -33,12 +36,12 @@ processRtrimOutput_PECBMS <- function(folder){
   # Determines which datafiles (combinations of species and stratum) have been analysed successfully. 
   # This information is found in "overview.csv", which has been produced by the script that called rtrim.
   
-  overview <- read.table("overview.csv",sep=";",dec=".",header = T)   #13/01/2023 Javier Rivas: Changes to produce outputs with dots - v1.6
+  overview <- read.table(paste0(folder, "/overview.csv"),sep=";",dec=".",header = T)   #13/01/2023 Javier Rivas: Changes to produce outputs with dots - v1.6
   listsuccessfulAnalyses <- overview$ss_combinations[overview$success == "yes"]
   listSpeciesStratumCombinations <- paste(listsuccessfulAnalyses, "_arg_input_stratum.csv", sep = "")
   
   # Determine how many combinations of species and stratum have been analysed successfully.
-  numberSpeciesStratumCombinations <- length (listSpeciesStratumCombinations)
+  numberSpeciesStratumCombinations <- length(listSpeciesStratumCombinations)
   
   # File with trends and indices for each combination of species and stratum
   all_Indices_All_Trends <- make_All_Indices_All_Trends(overview = overview, listSpeciesStratumCombinations = listSpeciesStratumCombinations)
@@ -51,15 +54,15 @@ processRtrimOutput_PECBMS <- function(folder){
     
     # The file with arguments contains the arguments to run the analysis for a particular combination of species and stratum (stratum is e.g. a region). 
     # These arguments are used when calling the function 'rtrim'.
-    arguments <- read.table(listSpeciesStratumCombinations[j], header = TRUE, stringsAsFactors = FALSE,sep=";",dec=".")
-    counts <- read.table(paste(arguments$File, "_counts.csv", sep = ""), header = TRUE,dec=".",sep=";")   #13/01/2023 Javier Rivas: Changes to produce outputs with dots - v1.6
+    arguments <- read.table(paste0(folder, "/", listSpeciesStratumCombinations[j]), header = TRUE, stringsAsFactors = FALSE,sep=";",dec=".")
+    counts <- read.table(paste0(folder, "/", arguments$File, "_counts.csv"), header = TRUE,dec=".",sep=";")   #13/01/2023 Javier Rivas: Changes to produce outputs with dots - v1.6
     #13/01/2023 : changes to accept either comma or dot as decimal symbol  - v1.6
     if(is.character(counts$count)==T){
-      counts <- read.table(paste(arguments$File, "_counts.csv", sep = ""), stringsAsFactors = FALSE, header = TRUE, dec = ",",sep=";")   
+      counts <- read.table(paste0(folder, "/", arguments$File, "_counts.csv"), stringsAsFactors = FALSE, header = TRUE, dec = ",",sep=";")   
     } 
     #13/01/2023 End of  changes
     # Also the result of the rtrim-analysis is required.
-    load(paste(arguments$File, ".RData", sep = "")) # produces object with name "result"
+    load(paste0(folder, "/", arguments$File, ".RData")) # produces object with name "result"
     
     #####################################################################################################################
     # CREATING FILES FOR LATER USE. 
@@ -88,7 +91,7 @@ processRtrimOutput_PECBMS <- function(folder){
     
     covariant_matrix <- vcov(result)
     
-    if (arguments$Save_fitted_values){
+    if(arguments$Save_fitted_values){
       
       FI <- results(result)
       
@@ -98,25 +101,25 @@ processRtrimOutput_PECBMS <- function(folder){
     # WRITING OUTPUT FILES. 
     #####################################################################################################################
     # Indices and time totals.
-    name_Indices_TT_file <- paste(arguments$File, "_indices_TT.csv", sep = "")
+    name_Indices_TT_file <- paste0(folder, "/", arguments$File, "_indices_TT.csv")
     write.table(indices_TT_file, name_Indices_TT_file, row.names = FALSE,sep=";",dec=".")   #13/01/2023 Javier Rivas: Changes to produce outputs with dots  - v1.6
     ############################################################ 
     # Slopes entire period and arguments.
-    name_arg_output_file <- paste(arguments$File, "_arg_output.csv", sep = "") 
+    name_arg_output_file <- paste0(folder, "/", arguments$File, "_arg_output.csv") 
     write.table(arg_output_file, name_arg_output_file, row.names = FALSE,sep=";",dec=".")   #13/01/2023 Javier Rivas: Changes to produce outputs with dots - v1.6
     ############################################################
     # Covariant matrix.
-    name_covariant_matrix <- paste(arguments$File, "_ocv.csv", sep = "")
+    name_covariant_matrix <- paste0(folder, "/", arguments$File, "_ocv.csv")
     write.table(covariant_matrix, name_covariant_matrix, row.names = FALSE,sep=";",dec=".")   #13/01/2023 Javier Rivas: Changes to produce outputs with dots - v1.6
     ############################################################
     # File with fitted values.
-    if (arguments$Save_fitted_values){
-      name_Fitted_Values_File <- paste(arguments$File, "_fitted_values.csv", sep = "")
+    if(arguments$Save_fitted_values){
+      name_Fitted_Values_File <- paste0(folder, "/", arguments$File, "_fitted_values.csv")
       write.table(FI, name_Fitted_Values_File, row.names = FALSE,sep=";",dec=".")  #13/01/2023 Javier Rivas: Changes to produce outputs with dots - v1.6
     }
     ############################################################
     # 25/01/2022 Javier Rivas: Generating Tables 1-3 for the PECBMS coordinators - v1.4
-    name_rawdata=paste(arguments$File, "_summarizing_tables.txt", sep = "")
+    name_rawdata=paste0(folder, "/", arguments$File, "_summarizing_tables.txt")
     # We are filling the  table on numbers of sites and counts
     write(paste0("\n", arguments$File, "\n"), file = name_rawdata)
     
@@ -204,9 +207,14 @@ processRtrimOutput_PECBMS <- function(folder){
   }
   
   all_Indices_All_Trends <- all_Indices_All_Trends[order(all_Indices_All_Trends$Species_number, all_Indices_All_Trends$Stratum_number, all_Indices_All_Trends$Recordtype_number), ]
-  write.table(all_Indices_All_Trends, "All_Indices_All_Trends.csv", row.names = FALSE,sep=";",dec=".")           # 09/04/2021 Eva Silarova changed "all" to capital "All": "all_Indices_All_Trends.csv" was changed to "All_Indices_All_Trends.csv" - v1.3
+  write.table(all_Indices_All_Trends, paste0(folder, "/All_Indices_All_Trends.csv"), row.names = FALSE,sep=";",dec=".")           # 09/04/2021 Eva Silarova changed "all" to capital "All": "all_Indices_All_Trends.csv" was changed to "All_Indices_All_Trends.csv" - v1.3
   ############################################################                                                  # 13/01/2023 Javier Rivas : Changes to produce outputs with dots
   
+  # ----------------------------------------------------------------------------
+  # END OF CODE FROM PECBMS
+  #-----------------------------------------------------------------------------
   
+  ## Return index trend data
+  return(all_Indices_All_Trends)
 }
 
